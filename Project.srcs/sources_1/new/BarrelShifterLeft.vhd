432b8bb6a4aaab0b6f963ce877a0547b7286c9ba
David Van Acken <davidvans.dv@gmail.com>
2024-03-01 13:53:01 -0800

    Remove process from barrel shifters

--- Project.srcs/sources_1/new/BarrelShifterLeft.vhd
+++ Project.srcs/sources_1/new/BarrelShifterLeft.vhd
@@ -14,36 +14,17 @@ architecture Behavioral of BarrelShifterLeft is
     signal  sh_8, sh_4, sh_2 : std_logic_vector(16 downto 0);
 
 begin
-    process(Sh_B, Sh_A)
-    begin
-        if (Sh_B(3)='1') then
-            sh_8(15 downto 8) <= Sh_A(7 downto 0);
-            sh_8(7 downto 0) <= "00000000";
-        else
-            sh_8(15 downto 0) <= Sh_A(15 downto 0);
-        end if;
-        
-        if (Sh_B(2)='1') then
-            sh_4(15 downto 4) <= sh_8(11 downto 0);
-            sh_4(3 downto 0) <= "0000";
-        else
-            sh_4(15 downto 0) <= sh_8(15 downto 0);
-        end if;
-        
-        if (Sh_B(1)='1') then
-            sh_2(15 downto 2) <= sh_4(13 downto 0);
-            sh_2(1 downto 0) <= "00";
-        else
-            sh_2(15 downto 0) <= sh_4(15 downto 0);
-        end if;
-        
-        if (Sh_B(0)='1') then
-            Sh_C(15 downto 1) <= sh_2(14 downto 0);
-            Sh_C(0) <= '0';
-        else
-            Sh_C(15 downto 0) <= sh_2(15 downto 0);
-        end if;
-        
-    end process;
-    
+
+    sh_8(15 downto 8) <= Sh_A(7 downto 0) when Sh_B(3)='1' else Sh_A(15 downto 8);
+    sh_8(7 downto 0) <= "00000000" when Sh_B(3)='1' else Sh_A(7 downto 0);
+            
+    sh_4(15 downto 4) <= sh_8(11 downto 0) when Sh_B(2)='1' else sh_8(15 downto 4);
+    sh_4(3 downto 0) <= "0000" when Sh_B(2)='1' else sh_8 (3 downto 0);
+
+    sh_2(15 downto 2) <= sh_4(13 downto 0) when Sh_B(1)='1' else sh_4(15 downto 2);
+    sh_2(1 downto 0) <= "00" when Sh_B(1)='1' else sh_4(1 downto 0);
+
+    Sh_C(15 downto 1) <= sh_2(14 downto 0) when Sh_B(0)='1' else sh_2(15 downto 1);
+    Sh_C(0) <= '0' when Sh_B(0)='1' else sh_2(0);
+            
 end Behavioral;
--- Project.srcs/sources_1/new/BarrelShifterRight.vhd
+++ Project.srcs/sources_1/new/BarrelShifterRight.vhd
@@ -14,36 +14,17 @@ architecture Behavioral of BarrelShifterRight is
     signal  sh_8, sh_4, sh_2 : std_logic_vector(16 downto 0);
 
 begin
-    process(Sh_B, Sh_A)
-    begin
-        if (Sh_B(3)='1') then
-            sh_8(7 downto 0) <= Sh_A(15 downto 8);
-            sh_8(15 downto 8) <= "00000000";
-        else
-            sh_8(15 downto 0) <= Sh_A(15 downto 0);
-        end if;
-        
-        if (Sh_B(2)='1') then
-            sh_4(11 downto 0) <= sh_8(15 downto 4);
-            sh_4(15 downto 12) <= "0000";
-        else
-            sh_4(15 downto 0) <= sh_8(15 downto 0);
-        end if;
-        
-        if (Sh_B(1)='1') then
-            sh_2(13 downto 0) <= sh_4(15 downto 2);
-            sh_2(15 downto 14) <= "00";
-        else
-            sh_2(15 downto 0) <= sh_4(15 downto 0);
-        end if;
-        
-        if (Sh_B(0)='1') then
-            Sh_C(14 downto 0) <= sh_2(15 downto 1);
-            Sh_C(15) <= '0';
-        else
-            Sh_C(15 downto 0) <= sh_2(15 downto 0);
-        end if;
-        
-    end process;
+
+    sh_8(7 downto 0) <= Sh_A(15 downto 8) when Sh_B(3)='1' else Sh_A (7 downto 0);
+    sh_8(15 downto 8) <= "00000000" when Sh_B(3)='1' else Sh_A (15 downto 8);
+    
+    sh_4(11 downto 0) <= sh_8(15 downto 4) when Sh_B(2)='1' else sh_8(11 downto 0);
+    sh_4(15 downto 12) <= "0000" when Sh_B(2)='1' else sh_8(15 downto 12);
+    
+    sh_2(13 downto 0) <= sh_4(15 downto 2) when Sh_B(1)='1' else sh_4(13 downto 0);
+    sh_2(15 downto 14) <= "00" when Sh_B(1)='1' else sh_4(15 downto 14);
+    
+    Sh_C(14 downto 0) <= sh_4(15 downto 1) when Sh_B(1)='1' else sh_4(14 downto 0);
+    Sh_C(15) <= '0' when Sh_B(1)='1' else sh_2(15);
     
 end Behavioral;
